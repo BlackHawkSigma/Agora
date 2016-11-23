42e444d512cb04a07c04d6cd49d284d0057b1a36
BlackHawkSigma <tobias.beese@gmx.de>
2016-11-23 20:42:58 +0100

    Add basic express scaffolding
    localhost:3000/ will render index.pug

---
+++ config/express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+module.exports = function() {
+  var app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+  app.set('view engine', 'pug');
+  app.set('views', './app/views');
+
+  app.use(express.static('./public'));
+
+  require('../app/routes/index.server.routes.js')(app);
+
+  return app;
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const debug = require('debug')('server');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
+
+var app = express();
+
+debug('starting express server');
+
+http.createServer(app).listen(3000, function() {
+  debug('express running');
+})
