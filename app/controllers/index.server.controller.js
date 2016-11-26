8445d81fccdce9e75f291cf746f1ad2cd8f41992
BlackHawkSigma <tobias.beese@gmx.de>
2016-11-26 22:27:16 +0100

    Change rendering index.pug
    Get title and content from express.

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,6 @@
 exports.render = function (req, res) {
-  res.render('index');
+  res.render('index', {
+    title: 'Paintshop 5 App',
+    content: 'this content is served from express.'
+  });
 };
