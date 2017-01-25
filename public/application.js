f5e0a24a4ad142144b0aba5320459a039f6ddeb3
Tobias Beese <tobias.beese@gmx.de>
2017-01-25 09:55:10 +0100

    Create cosmino module

--- public/application.js
+++ public/application.js
@@ -1,8 +1,6 @@
 var mainAppModuleName = 'paintlineApp';
 
-// var mainAppModule = angular.module(mainAppModuleName, ['ng-route', 'angular-loading-bar']);
-
-var mainAppModule = angular.module(mainAppModuleName, []);
+var mainAppModule = angular.module(mainAppModuleName, ['angular-loading-bar', 'cosmino']);
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainAppModuleName]);
