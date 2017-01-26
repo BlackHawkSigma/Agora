d6d648fc9744a8585c4255610f52df003b4b0f5c
Tobias Beese <tobias.beese@gmx.de>
2017-01-26 14:23:17 +0100

    Add gracefull shutdown on SIGINT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const debug = require('debug')('server');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express');
-var mysql = require('./config/mysql');
+var mysql = require('./config/mysql')();
 
 var app = express();
 
@@ -12,4 +12,25 @@ debug('starting express server');
 
 http.createServer(app).listen(3000, function() {
   debug('express running');
+})
+
+// crtl+c in Windows richtig interpretieren
+if (process.platform === "win32") {
+  var rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on("SIGINT", function () {
+    process.emit("SIGINT");
+  });
+}
+
+// gracefully shut down everything
+process.on('SIGINT', function() {
+  debug('exiting...');
+  mysql.end(function() {
+    debug('db connection closed');
+    process.exit();
+  });
 })
