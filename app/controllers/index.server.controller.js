860f7990ecf9784988fe18ffc6b600768ecd71ae
Tobias Beese <tobias.beese@gmx.de>
2017-01-27 14:45:59 +0100

    Remove content for index render

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,6 +1,5 @@
 exports.render = function (req, res) {
   res.render('index', {
-    title: 'Paintshop 5 App',
-    content: 'this content is served from express.'
+    title: 'Paintshop 5 App'
   });
 };
