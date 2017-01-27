12d202cf31707e1752cf7b407232bb27d0d191bc
Tobias Beese <tobias.beese@gmx.de>
2017-01-27 14:36:13 +0100

    Remove seperate farbton view

--- app/routes/farbton.server.routes.js
+++ app/routes/farbton.server.routes.js
@@ -1,5 +1,4 @@
 module.exports = function(app) {
   var farbton = require('../../app/controllers/farbton.server.controller');
   app.route('/farbtonListe').get(farbton.list);
-  app.route('/farbton').get(farbton.render);
 };
