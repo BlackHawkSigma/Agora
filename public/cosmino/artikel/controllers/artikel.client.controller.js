5cb5d155ebf0f1058d9d6b7ccdd2b5d20f814357
BlackHawkSigma <tobias.beese@gmx.de>
2017-01-30 19:56:41 +0100

    Add alerts for database error

--- public/cosmino/artikel/controllers/artikel.client.controller.js
+++ public/cosmino/artikel/controllers/artikel.client.controller.js
@@ -1,6 +1,10 @@
 angular.module('artikel').controller('ArtikelCtrl', ['$scope', '$routeParams', '$location', 'Artikel',
 function($scope, $routeParams, $location, Artikel) {
   $scope.find = function() {
-    $scope.artikel = Artikel.query();
+    Artikel.query(function(data) {
+      $scope.artikel = data;
+    }, function(err) {
+      $scope.err = err;
+    });
   };
 }]);
--- public/cosmino/farbton/controllers/farbton.client.controller.js
+++ public/cosmino/farbton/controllers/farbton.client.controller.js
@@ -1,6 +1,10 @@
 angular.module('farbton').controller('FarbtonCtrl', ['$scope', '$routeParams', '$location', 'Farbton',
 function($scope, $routeParams, $location, Farbton) {
   $scope.find = function() {
-    $scope.colors = Farbton.query();
+    Farbton.query(function(data) {
+      $scope.colors = data;
+    }, function(err) {
+      $scope.err = err;
+    });
   };
 }]);
