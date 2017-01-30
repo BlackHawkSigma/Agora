c7c01cd1186fb47b4427bc582d7f4d5d2f20263a
BlackHawkSigma <tobias.beese@gmx.de>
2017-01-30 21:08:54 +0100

    Change timepicker design

--- public/cosmino/controllers/cosmino.client.controller.js
+++ public/cosmino/controllers/cosmino.client.controller.js
@@ -1,31 +1,34 @@
-angular.module('cosmino').controller('TimepickerDemoCtrl', function ($scope) {
-  var time = new Date();
-  $scope.mytime = time.setMinutes(0);
+angular.module('cosmino').controller('TimepickerCtrl', function ($scope, uibDateParser) {
+  $scope.dateFormat = 'dd.MM.yy HH:mm'
+
+  $scope.isCollapsed = true;
+
+  $scope.endTime = new Date().setSeconds(0);
+
+  $scope.startTime = (function(){
+    this.setDate(this.getDate()-1);
+    this.setMinutes(0);
+    this.setSeconds(0);
+    return this
+  }).call(new Date());
 
   $scope.hstep = 1;
   $scope.mstep = 15;
-
   $scope.ismeridian = false;
-  $scope.toggleMode = function() {
-    $scope.ismeridian = ! $scope.ismeridian;
-  };
 
   $scope.setEarly = function() {
-    var d = new Date();
-    d.setHours( 6 );
-    d.setMinutes( 0 );
-    $scope.mytime = d;
+    $scope.startTime = new Date($scope.startTime).setHours(6);
+    $scope.endTime = new Date($scope.endTime).setHours(14);
   };
   $scope.setLate = function() {
-    var d = new Date();
-    d.setHours( 14 );
-    d.setMinutes( 0 );
-    $scope.mytime = d;
+    $scope.startTime = new Date($scope.startTime).setHours(14);
+    $scope.endTime = new Date($scope.endTime).setHours(22);
   };
   $scope.setNight = function() {
-    var d = new Date();
-    d.setHours( 22 );
-    d.setMinutes( 0 );
-    $scope.mytime = d;
+    $scope.startTime = new Date($scope.startTime).setHours(22);
+    $scope.endTime = new Date($scope.endTime).setHours(6);
   };
+  $scope.updateTimes = function() {
+    $scope.isCollapsed =true;
+  }
 });
