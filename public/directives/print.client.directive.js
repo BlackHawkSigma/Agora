6bdad4eb86f09cb556054c2ecd70eee546a86e29
BlackHawkSigma <tobias.beese@gmx.de>
2017-02-09 19:14:30 +0100

    Add print directive

---
+++ public/directives/print.client.directive.js
@@ -0,0 +1,36 @@
+angular.module('paintlineApp').directive('ngPrint', [function() {
+  var printSection = document.getElementById('printSection');
+
+  // If there is no printing section, create one
+  if (!printSection) {
+    printSection = document.createElement('div');
+    printSection.id = 'printSection';
+    document.body.appendChild(printSection);
+  }
+
+  function link(scope, element, attrs) {
+    element.on('click', function() {
+      var elementToPrint = document.getElementById(attrs.printElementId);
+      if (elementToPrint) {
+        printElement(elementToPrint);
+        window.print();
+      }
+    });
+
+    window.onafterprint = function() {
+      //  Clean the print section
+      printSection.innerHTML = '';
+    }
+  }
+
+  function printElement(element) {
+    // Clone the printable element
+    var domClone = element.cloneNode(true);
+    printSection.appendChild(domClone);
+  }
+
+  return {
+    link: link,
+    restrict: 'A'
+  };
+}]);
