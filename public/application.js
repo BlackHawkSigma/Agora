5c9df085448da647b1cd063c6bf4c593ea146464
Tobias Beese <tobias.beese@gmx.de>
2017-02-14 13:25:58 +0100

    Change menue order

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainAppModuleName = 'paintlineApp';
 
-var mainAppModule = angular.module(mainAppModuleName, ['angular-loading-bar', 'ngRoute', 'ngResource', 'cosmino', 'artikel', 'farbton', 'rejections', 'dashboard']);
+var mainAppModule = angular.module(mainAppModuleName, ['angular-loading-bar', 'ngRoute', 'ngResource', 'cosmino', 'rejections', 'dashboard', 'artikel', 'farbton']);
 
 angular.element(document).ready(function() {
   moment.locale('de');
