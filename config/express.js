4fe6d7e460802018362d72ae1605ce97453389de
Tobias Beese <tobias.beese@gmx.de>
2017-02-17 08:37:20 +0100

    Add selection for port based on ENV

--- config/express.js
+++ config/express.js
@@ -11,6 +11,7 @@ module.exports = function() {
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
   } else if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('common'));
     app.use(compress());
   }
 
